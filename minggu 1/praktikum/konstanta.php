bb8a11352bc0484ce897d772d7e5e1ea054fea67
ArdaFadli <fadlynasya@gmail.com>
2022-03-16 22:00:48 +0700

    message

---
+++ minggu 1/praktikum/konstanta.php
@@ -0,0 +1,6 @@
+<?
+define ("NAMA", "ARDA ");
+define ("NILAI", 85);
+echo "Nama : " . NAMA;
+echo "<br>Nilai : " . NILAI;
+?>
---
+++ minggu 1/praktikum/variabel.php
@@ -0,0 +1,6 @@
+<?php
+$nim = "A11.2020.13087";
+$nama = 'ARDA';
+echo "NIM : " . $nim . "<br>";
+echo "Nama : $nama";
+?>
